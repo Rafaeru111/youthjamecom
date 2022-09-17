86fb1dbed73e27b00393fe7076dc0d5d518667e1
Your Name <you@example.com>
2022-09-17 15:24:51 +0800

    first try

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+
+
+    public function handle(Request $request, Closure $next)
+    {
+
+   //This is to check the auth if the role of a use is admin or not
+       if(Auth::check())
+        {
+            if(Auth::user()->role_as == '1')
+            { 
+                return $next($request);
+            }
+            else
+            {
+                return redirect('/home')->with('status','Access Denied! You are not the Admin');
+            }
+        }
+        else
+        {
+            return redirect('/home')->with('status','Please Login First');
+        }
+
+
+    }
+}
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    
+    
+    <!-- Custom fonts for this template-->
+    <link href="{{ asset('admin/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+
+    <!-- Custom styles for this template-->
+    <link href="{{ asset('admin/css/sb-admin-2.min.css') }}" rel="stylesheet">
+    
+
+
+</head>
+<body id="page-top">
+
+        <div id="wrapper">
+             @include('layouts.inc.sidebar')
+
+
+             <div  id="content-wrapper" class="d-flex flex-column">
+                
+            @include('layouts.inc.adminnav')
+
+            @yield('content')
+        
+            @include('layouts.inc.adminfooter')
+
+            </div>
+     
+              
+
+
+        </div>
+            
+    
+
+    
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{ asset('admin/vendor/jquery/jquery.min.js') }}" defer></script>
+    <script src="{{ asset('admin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}" defer></script>
+
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{ asset('admin/vendor/jquery-easing/jquery.easing.min.js') }}" defer></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{ asset('admin/js/sb-admin-2.min.js') }}" defer></script>
+
+
+    <!-- Page level plugins -->
+    <script src="{{ asset('admin/vendor/chart.js/Chart.min.js') }}" defer></script>
+
+    <!-- Page level custom scripts -->
+    <script src="{{ asset('admin/js/demo/chart-area-demo.js') }}" defer></script>
+    <script src="{{ asset('admin/js/demo/chart-pie-demo.js') }}" defer></script>
+
+@yield('scripts')
+</body>
+</html>
+ 
